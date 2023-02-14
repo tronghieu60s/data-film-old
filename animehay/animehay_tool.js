a4737afc1fb7e71d5871d2c5617a594af8007f2b
Trong Hieu <tronghieu60s@gmail.com>
2023-02-14 13:10:49 +0700

    update data

--- animehay/animehay_tool.js
+++ animehay/animehay_tool.js
@@ -1,10 +1,10 @@
 const fs = require("fs");
 const papa = require("papaparse");
-const { PathTrackingData, PathWatchData } = require("./core/const");
+const { PathPostData, PathWatchData } = require("./core/const");
 
 async function getGroup() {
-  const trackData = papa.parse(
-    fs.readFileSync(PathTrackingData, { flag: "r", encoding: "utf8" }),
+  const postData = papa.parse(
+    fs.readFileSync(PathPostData, { flag: "r", encoding: "utf8" }),
     { header: true, skipEmptyLines: true }
   ).data;
 
@@ -15,17 +15,31 @@ async function getGroup() {
     })
     .data.reduce((cur, item) => {
       if (!cur[item.movieId]) cur[item.movieId] = [];
-      cur[item.movieId].push(item);
+      const { movieEp, movieHdx } = item;
+      cur[item.movieId].push({
+        episode_name: movieEp,
+        episode_slug: `tap-${movieEp}`,
+        video_hydrax: movieHdx,
+      });
       return cur;
     }, {});
 
-  for (let index = 0; index < trackData.length; index += 1) {
-    trackData[index].movieEpisodes = `"${JSON.stringify(
-      watchData[trackData[index].movieId] || []
-    )}"`;
+  for (let index = 0; index < postData.length; index += 1) {
+    delete postData[index].movieLink;
+    postData[index].movieUniqueId = `animehay-${postData[index].movieId}`;
+    postData[index].movieTags = postData[index]?.movieOriginalName
+      .split(",")
+      .join("|")
+      .split("/")
+      .join("|");
+    postData[index].movieCountry =
+      postData[index].movieCategories.indexOf("CN Animation") > -1 ? "Trung Quốc" : "Nhật Bản";
+    postData[index].movieEpisodes = `${JSON.stringify(
+      watchData[postData[index].movieId] || []
+    )}`;
   }
 
-  return trackData;
+  return postData;
 }
 
 module.exports = { getGroup };
